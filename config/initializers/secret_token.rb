9aee79187e579f2ed76b10155b75597fdc41de88
Florian MARIN <fmarin@octo.com>
2013-04-09 22:26:31 +0200

    Initialisation application Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mesinvites::Application.config.secret_token = '0d47077392081e2fecc85a9bb89f871e773ecc1138f2a796243d00bc2c40eb1410373cb6c2d7c97b18b45fff6561b21761707aad1a34a474310bd0f15f1d6827'
