ed186ac368b1fd60a7733076fa3b3304d90de611
Florian MARIN <fmarin@octo.com>
2013-04-14 21:54:20 +0200

    Suppression de la page d'accueil "Home" inutile -> Par défaut : Liste des repas Tout est en français

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Mesinvites::Application.routes.draw do
   resources :meals
 
 
-  get "home/index"
+  get "meals/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -53,7 +53,7 @@ Mesinvites::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'meals#index'
 
   # See how all your routes lay out with "rake routes"
 
