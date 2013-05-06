368bf6f17a48d64eeb7f95581effb7cc89b31ae3
Florian MARIN <fmarin@octo.com>
2013-05-07 00:51:21 +0200

    Implémentation de la recherche exacte des repas par nom d'invité

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,4 +2,8 @@ class Meal < ActiveRecord::Base
 	attr_accessible :meal_date, :diner, :aperitif, :starter, :course, :cheese, :salad, :dessert, :notes
 	
 	belongs_to :guest
+
+	def self.search(query)
+		Meal.joins(:guest).where('guests.name' => query)
+	end
 end
--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -15,4 +15,15 @@ describe Meal do
 		Meal.all.should eq([meal])
 	end
 
+	it 'returns all meals who participates the guest "Toto" when "Toto" is searched' do
+		guestToto = FactoryGirl.create(:guest, name: "Toto")
+		guestTata = FactoryGirl.create(:guest, name: "Tata")
+		
+		mealOk1 = FactoryGirl.create(:meal, guest: guestToto)
+		mealKo = FactoryGirl.create(:meal, guest: guestTata)
+		mealOk2 = FactoryGirl.create(:meal, guest: guestToto)
+	
+		Meal.search("Toto").should eq([mealOk1, mealOk2])
+	end
+
 end
